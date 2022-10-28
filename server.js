b53e57eaaa246738e0ed0c666ba6d1735032b47e
anajembaedwin <anajembaedwin@gmail.com>
2022-10-28 23:02:19 +0100

    updated server to get json

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 
-const app = express()
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({ 
+    "slackUsername": "iSommie", 
+    "backend": true, 
+    "age": 27, 
+    "bio": "I am a budding Backend Developer who enjoys watching sci-fi movies" 
+    });
+})
+
+app.set('json spaces', 40)
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
